836e007e1223cfaf24afe2aa87c273718267948f
MilanRegmiGitH <milanregmi561@gmail.com>
2025-02-26 19:35:11 +0545

    some small fix

--- frontend/frontend/tailwind.config.js
+++ frontend/frontend/tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     darkMode: ["class"],
-    content:["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+    content:["./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+],
   theme: {
   	extend: {
   		borderRadius: {
@@ -53,6 +56,6 @@ module.exports = {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [],
 }
 
